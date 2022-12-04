1d8ab97a10c17e181f3aa7260a241e300ffa2e59
soyeon0725 <soyeon.b0725@gmail.com>
2022-12-04 20:04:50 +0900

    MyPage - antd Tabs - basic 연동   - 탭 4개 타이틀 노출   - 찜한 포스트, 구매내역 탭 => TextList 컴포넌트 연동
    components
    - list > TextList 생성
    - antd Tabs - basic 연동 (이미지 행 분기 처리 => 찜한 포스트 : 미 노출, 구매내역 : 노출)

---
+++ src/components/list/TextList.jsx
@@ -0,0 +1,169 @@
+import {Image, Space, Table, Tag} from 'antd';
+
+const TextList = (props) => {
+    console.log(props)
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            render: (text) => <a>{text}</a>,
+        },
+        {
+            title: 'Age',
+            dataIndex: 'age',
+            key: 'age',
+        },
+        {
+            title: 'Address',
+            dataIndex: 'address',
+            key: 'address',
+        },
+        {
+            title: 'Tags',
+            key: 'tags',
+            dataIndex: 'tags',
+            render: (_, { tags }) => (
+                <>
+                    {tags.map((tag) => {
+                        let color = tag.length > 5 ? 'geekblue' : 'green';
+                        if (tag === 'loser') {
+                            color = 'volcano';
+                        }
+                        return (
+                            <Tag color={color} key={tag}>
+                                {tag.toUpperCase()}
+                            </Tag>
+                        );
+                    })}
+                </>
+            ),
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => (
+                <Space size="middle">
+                    <a>Invite {record.name}</a>
+                    <a>Delete</a>
+                </Space>
+            ),
+        },
+    ];
+    const data = [
+        {
+            key: '1',
+            name: 'John Brown',
+            age: 32,
+            address: 'New York No. 1 Lake Park',
+            tags: ['nice', 'developer'],
+        },
+        {
+            key: '2',
+            name: 'Jim Green',
+            age: 42,
+            address: 'London No. 1 Lake Park',
+            tags: ['loser'],
+        },
+        {
+            key: '3',
+            name: 'Joe Black',
+            age: 32,
+            address: 'Sidney No. 1 Lake Park',
+            tags: ['cool', 'teacher'],
+        },
+    ];
+    const imgColumns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            render: (text) => <a>{text}</a>,
+        },
+        {
+            title: 'Age',
+            dataIndex: 'age',
+            key: 'age',
+        },
+        {
+            title: 'Address',
+            dataIndex: 'address',
+            key: 'address',
+        },
+        {
+            title: 'Tags',
+            key: 'tags',
+            dataIndex: 'tags',
+            render: (_, { tags }) => (
+                <>
+                    {tags.map((tag) => {
+                        let color = tag.length > 5 ? 'geekblue' : 'green';
+                        if (tag === 'loser') {
+                            color = 'volcano';
+                        }
+                        return (
+                            <Tag color={color} key={tag}>
+                                {tag.toUpperCase()}
+                            </Tag>
+                        );
+                    })}
+                </>
+            ),
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => (
+                <Space size="middle">
+                    <a>Invite {record.name}</a>
+                    <a>Delete</a>
+                </Space>
+            ),
+        },
+        {
+            title: 'Image',
+            key: 'image',
+            render: (_, {image}) => (
+                <Image
+                    width={100}
+                    height={100}
+                    src={image}
+                />
+            ),
+        },
+    ];
+    const imgData = [
+        {
+            key: '1',
+            name: 'John Brown',
+            age: 32,
+            address: 'New York No. 1 Lake Park',
+            tags: ['nice', 'developer'],
+            image: 'https://cdn.pixabay.com/photo/2016/07/14/13/35/mountains-1516733_1280.jpg'
+        },
+        {
+            key: '2',
+            name: 'Jim Green',
+            age: 42,
+            address: 'London No. 1 Lake Park',
+            tags: ['loser'],
+            image: 'https://cdn.pixabay.com/photo/2016/07/14/13/35/mountains-1516733_1280.jpg'
+        },
+        {
+            key: '3',
+            name: 'Joe Black',
+            age: 32,
+            address: 'Sidney No. 1 Lake Park',
+            tags: ['cool', 'teacher'],
+            image: 'https://cdn.pixabay.com/photo/2016/07/14/13/35/mountains-1516733_1280.jpg'
+        },
+    ];
+
+    return (
+        <Table
+            columns={props.isImage ? imgColumns : columns}
+            dataSource={props.isImage ? imgData : data}
+        />
+    );
+}
+export default TextList;
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,6 +1,38 @@
+import { Tabs } from 'antd';
+import TextList from "../components/list/TextList";
+
 const MyPage = () => {
+    const onChange = (key) => {
+        console.log(key);
+    };
     return (
-        <div>마이페이지</div>
+        <Tabs
+            style={{ alignItems: 'center' }}
+            defaultActiveKey="1"
+            onChange={onChange}
+            items={[
+                {
+                    label: `전체 포스트`,
+                    key: '1',
+                    children: `전체 포스트 영역`,
+                },
+                {
+                    label: `찜한 포스트`,
+                    key: '2',
+                    children: <TextList />,
+                },
+                {
+                    label: `구매내역`,
+                    key: '3',
+                    children: <TextList isImage={true} />,
+                },
+                {
+                    label: `대시보드`,
+                    key: '4',
+                    children: `대시보드 영역`,
+                },
+            ]}
+        />
     );
 }
 export default MyPage;
