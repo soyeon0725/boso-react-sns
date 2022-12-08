01fa9262420fff66d3567bafc78819f9ca84ff1e
soyeon0725 <soyeon.b0725@gmail.com>
2022-12-08 22:41:08 +0900

    로그인 버튼 클릭 시 id, photo 리덕스 연동(personalInfo)

--- src/app/slice.js
+++ src/app/slice.js
@@ -3,17 +3,22 @@ import {createSlice} from "@reduxjs/toolkit";
 const exampleSlice = createSlice({
     name: "exampleInfo",
     initialState: {
-        list : []
+        list : [],
+        동: {}
     },
     reducers: {
         setList: (state, action) => {
             state.list = action.payload;
-        }
+        },
+        setPersonalInfo: (state, action) => {
+            state.personalInfo = action.payload;
+        },
     }
 });
 
-export const {setList} = exampleSlice.actions;
+export const {setList, setPersonalInfo} = exampleSlice.actions;
 
 export const selectList = state => state.exampleInfo.list;
+export const selectPersonalInfo = state => state.exampleInfo.personalInfo;
 
 export {exampleSlice};
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import {useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
+import {useDispatch} from "react-redux";
+import {setPersonalInfo} from "../app/slice";
 
 import { firestore } from "../firebase/Firebase";
 import { Button, Form, Input } from 'antd';
@@ -8,27 +10,31 @@ import Default from "../modal/Default";
 // firebase 이메일 & 비밀번호 로그인 연동
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
 
+
 const Login = () => {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
     const [defaultModal, setDefaultModal] = useState({show: false, type: ''});
 
     const onFinish = (values) => {
         console.log('Success:', values);
         const user = firestore.collection("user");
-        let userList = [{
-            id: '',
-            password: ''
-        }];
+        let userList = [];
         user.get().then((docs) => {
             docs.forEach((doc) => {
                 userList.push({
                     id: doc.data().id,
-                    password: doc.data().password
+                    password: doc.data().password,
+                    photo: doc.data().photo
                 });
             })
             let isUser = false;
             for (let i = 0; i < userList.length; i++) {
                 if (values.username === userList[i].id && values.password === userList[i].password) {
+                    dispatch(setPersonalInfo({
+                        id: userList[i].id,
+                        photo: userList[i].photo
+                    }));
                     isUser = true;
                 }
             }
@@ -43,87 +49,84 @@ const Login = () => {
     const goToJoin = () => navigate('/join');
 
     // 이메일 & 비밀번호 로그인 테스트 (12/6)
-    const auth = getAuth();
-
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [userEmail, setUserEmail] = useState('');
-    const [userPw, setUserPw] = useState('');
-    const [login, setLogin] = useState('');
-
-    const onChangeEmail = e => setEmail(e.target.value);
-    const onChangePw = e => setPassword(e.target.value);
-    const ChangeUserEmail = e => setUserEmail(e.target.value);
-    const ChangeUserPw = e => setUserPw(e.target.value);
-
-    const createUser = e => {
-        e.preventDefault();
-
-        // 1. 신규 사용자 가입
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed in
-                const user = userCredential.user;
-                console.log("신규 사용자 가입 성공");
-                console.log(user);
-                setEmail('');
-                setPassword('');
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log("신규 사용자 가입 실패");
-                console.log(errorCode, errorMessage);
-            })
-    };
-
-    const signIn = e => {
-        e.preventDefault();
-        // 2. 기존 사용자 로그인
-        signInWithEmailAndPassword(auth, userEmail, userPw)
-            .then((userCredential) => {
-                // Signed in
-                const user = userCredential.user;
-                console.log("기존 사용자 로그인 성공");
-                console.log(user);
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log("기존 사용자 로그인 실패");
-                console.log(errorCode, errorMessage);
-            });
-    };
-
-    // 3. 인증 상태 관찰자 설정 및 사용자 데이터 가져오기
-    // 임시로 useEffect 처리
-    useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                // User is signed in, see docs for a list of available properties
-                // https://firebase.google.com/docs/reference/js/firebase.User
-                const uid = user.uid;
-                console.log("로그인 상태");
-                setLogin(user.email);
-            } else {
-                // User is signed out
-                console.log("로그아웃 상태");
-                setLogin('');
-            }
-        });
-    }, []);
-
-    const logOut = e => {
-        e.preventDefault();
-        // 4. 사용자를 로그아웃하려면 signOut 을 호출
-        signOut(auth).then(() => {
-            // Sign-out successful.
-            console.log('로그아웃 성공');
-        }).catch((error) => {
-            // An error happened.
-            console.log('로그아웃 실패');
-        });
-    };
+    // const auth = getAuth();
+    //
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
+    // const [userEmail, setUserEmail] = useState('');
+    // const [userPw, setUserPw] = useState('');
+    // const [login, setLogin] = useState('');
+    //
+    // const onChangeEmail = e => setEmail(e.target.value);
+    // const onChangePw = e => setPassword(e.target.value);
+    // const ChangeUserEmail = e => setUserEmail(e.target.value);
+    // const ChangeUserPw = e => setUserPw(e.target.value);
+    //
+    // const createUser = e => {
+    //     e.preventDefault();
+    //
+    //     // 1. 신규 사용자 가입
+    //     createUserWithEmailAndPassword(auth, email, password)
+    //         .then((userCredential) => {
+    //             // Signed in
+    //             const user = userCredential.user;
+    //             console.log("신규 사용자 가입 성공");
+    //             console.log(user);
+    //             setEmail('');
+    //             setPassword('');
+    //         })
+    //         .catch((error) => {
+    //             const errorCode = error.code;
+    //             const errorMessage = error.message;
+    //             console.log("신규 사용자 가입 실패");
+    //             console.log(errorCode, errorMessage);
+    //         })
+    // };
+    // const signIn = e => {
+    //     e.preventDefault();
+    //     // 2. 기존 사용자 로그인
+    //     signInWithEmailAndPassword(auth, userEmail, userPw)
+    //         .then((userCredential) => {
+    //             // Signed in
+    //             const user = userCredential.user;
+    //             console.log("기존 사용자 로그인 성공");
+    //             console.log(user);
+    //         })
+    //         .catch((error) => {
+    //             const errorCode = error.code;
+    //             const errorMessage = error.message;
+    //             console.log("기존 사용자 로그인 실패");
+    //             console.log(errorCode, errorMessage);
+    //         });
+    // };
+    // // 3. 인증 상태 관찰자 설정 및 사용자 데이터 가져오기
+    // // 임시로 useEffect 처리
+    // useEffect(() => {
+    //     onAuthStateChanged(auth, (user) => {
+    //         if (user) {
+    //             // User is signed in, see docs for a list of available properties
+    //             // https://firebase.google.com/docs/reference/js/firebase.User
+    //             const uid = user.uid;
+    //             console.log("로그인 상태");
+    //             setLogin(user.email);
+    //         } else {
+    //             // User is signed out
+    //             console.log("로그아웃 상태");
+    //             setLogin('');
+    //         }
+    //     });
+    // }, []);
+    // const logOut = e => {
+    //     e.preventDefault();
+    //     // 4. 사용자를 로그아웃하려면 signOut 을 호출
+    //     signOut(auth).then(() => {
+    //         // Sign-out successful.
+    //         console.log('로그아웃 성공');
+    //     }).catch((error) => {
+    //         // An error happened.
+    //         console.log('로그아웃 실패');
+    //     });
+    // };
 
     return (
         <>
@@ -190,46 +193,46 @@ const Login = () => {
                     {defaultModal.show && <Default defaultModal={defaultModal} setDefaultModal={setDefaultModal} />}
                 </Form>
             </div>
-            <div>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={onChangeEmail}
-                    />
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={onChangePw}
-                    />
-                    <button onClick={createUser}>click</button>
-                </div>
-                <div>
-                    <label htmlFor="email">userEmail</label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={userEmail}
-                        onChange={ChangeUserEmail}
-                    />
-                    <label htmlFor="password">userPassword</label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={userPw}
-                        onChange={ChangeUserPw}
-                    />
-                    <button onClick={signIn}>Login</button>
-                </div>
-                <div>
-                    user login : {login}
-                </div>
-                <button onClick={logOut}>Logout</button>
-            </div>
+            {/*<div>*/}
+            {/*    <div>*/}
+            {/*        <label htmlFor="email">Email</label>*/}
+            {/*        <input*/}
+            {/*            type="email"*/}
+            {/*            id="email"*/}
+            {/*            value={email}*/}
+            {/*            onChange={onChangeEmail}*/}
+            {/*        />*/}
+            {/*        <label htmlFor="password">Password</label>*/}
+            {/*        <input*/}
+            {/*            type="password"*/}
+            {/*            id="password"*/}
+            {/*            value={password}*/}
+            {/*            onChange={onChangePw}*/}
+            {/*        />*/}
+            {/*        <button onClick={createUser}>click</button>*/}
+            {/*    </div>*/}
+            {/*    <div>*/}
+            {/*        <label htmlFor="email">userEmail</label>*/}
+            {/*        <input*/}
+            {/*            type="email"*/}
+            {/*            id="email"*/}
+            {/*            value={userEmail}*/}
+            {/*            onChange={ChangeUserEmail}*/}
+            {/*        />*/}
+            {/*        <label htmlFor="password">userPassword</label>*/}
+            {/*        <input*/}
+            {/*            type="password"*/}
+            {/*            id="password"*/}
+            {/*            value={userPw}*/}
+            {/*            onChange={ChangeUserPw}*/}
+            {/*        />*/}
+            {/*        <button onClick={signIn}>Login</button>*/}
+            {/*    </div>*/}
+            {/*    <div>*/}
+            {/*        user login : {login}*/}
+            {/*    </div>*/}
+            {/*    <button onClick={logOut}>Logout</button>*/}
+            {/*</div>*/}
         </>
 
     );
